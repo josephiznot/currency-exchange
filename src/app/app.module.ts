7152b3dc535535bcd4597e45988ae67af91a4d96
josephiznot <josephiznot@gmail.com>
2018-07-29 12:55:25 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { CalculatorComponent } from "./calculator/calculator.component";
+import { HeaderComponent } from "./header/header.component";
+import { DataComponent } from "./data/data.component";
+import { GraphComponent } from "./graph/graph.component";
+import { InputsComponent } from "./inputs/inputs.component";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    HeaderComponent,
+    DataComponent,
+    GraphComponent,
+    InputsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
